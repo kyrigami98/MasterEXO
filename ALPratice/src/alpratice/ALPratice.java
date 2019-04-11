3e9b37378e78fddb47fee06bce41b615bc6c6994
kyrigami98 <31952492+kyrigami98@users.noreply.github.com>
2019-04-11 11:12:41 +0200

    TPJAVACoursArchitectureLogiciel

---
+++ ALPratice/src/alpratice/ALPratice.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alpratice;
+
+/**
+ *
+ * @author My Sweet House
+ */
+public class ALPratice {
+      String client;
+      String Server;
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getServer() {
+        return Server;
+    }
+
+    public void setServer(String Server) {
+        this.Server = Server;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void void main(String[] args) {
+     
+     
+    }
+
+    public ALPratice() {
+        //constructeur
+    }
+    
+}
